00c943e54e651aa8bcc8ea16524a64e4e557e447
Hoon Lee <yl.hoony@gmail.com>
2017-11-08 08:19:41 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -55,13 +55,13 @@ def monopoly_with_fourth_tier
           "mortgage_value"=>"$100"
         },
         pennsylvania_railroad: {
-          mortgage_value: "$200"
+          "mortgage_value"=>"$200"
         },
         b_and_o_railroad: {
-          mortgage_value: "$400"
+          "mortgage_value"=>"$400"
         },
         shortline: {
-          mortgage_value: "$800"
+          "mortgage_value"=>"$800"
         }
       },
       rent_in_dollars: {
