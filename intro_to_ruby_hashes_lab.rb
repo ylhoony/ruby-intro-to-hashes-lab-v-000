42175c687d5a25aef57f251e2b15f7e27cd2a799
Hoon Lee <yl.hoony@gmail.com>
2017-11-08 08:04:51 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,9 +30,8 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4,
       names: {
-        reading_railroad: {
-          pennsylvania_railroad: {}
-        }
+        reading_railroad: {},
+        pennsylvania_railroad: {}
       },
       rent_in_dollars: {
         one_piece_owned: 25,
